f14b1325f6b87bb22fb5a0a32000b1aa84d2e943
Chris S <mrshrinkray@gmail.com>
2017-03-01 12:05:01 +0000

    AttachmentLinkConverter and tests

---
+++ src/Roadkill.Core/Text/Parsers/Links/Converters/SpecialLinkConverter.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Web.Mvc;
+
+namespace Roadkill.Core.Text.Parsers.Links.Converters
+{
+	public class SpecialLinkConverter : IHtmlLinkTagConverter
+	{
+		private readonly UrlHelper _urlHelper;
+
+		public SpecialLinkConverter(UrlHelper urlHelper)
+		{
+			_urlHelper = urlHelper;
+		}
+
+		public bool IsMatch(HtmlLinkTag htmlLinkTag)
+		{
+			if (htmlLinkTag == null)
+				return false;
+
+			string href = htmlLinkTag.OriginalHref;
+			string lowerHref = href?.ToLower() ?? "";
+
+			return lowerHref.StartsWith("special:");
+		}
+
+		public HtmlLinkTag Convert(HtmlLinkTag htmlLinkTag)
+		{
+			string href = htmlLinkTag.OriginalHref;
+			string lowerHref = href?.ToLower();
+
+			if (!lowerHref.StartsWith("special:"))
+				return htmlLinkTag;
+
+			htmlLinkTag.Href = _urlHelper.Content("~/wiki/" + href);
+
+			return htmlLinkTag;
+		}
+	}
+}
---
+++ src/Roadkill.Tests/Unit/Text/Parsers/Links/Converters/SpecialLinkConverterTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using Roadkill.Core.Text.Parsers.Links;
+using Roadkill.Core.Text.Parsers.Links.Converters;
+using Roadkill.Tests.Unit.StubsAndMocks;
+
+namespace Roadkill.Tests.Unit.Text.Parsers.Links.Converters
+{
+	public class SpecialLinkConverterTests
+	{
+		private UrlHelperMock _urlHelperMock;
+		private SpecialLinkConverter _converter;
+
+		[SetUp]
+		public void Setup()
+		{
+			_urlHelperMock = new UrlHelperMock();
+			_converter = new SpecialLinkConverter(_urlHelperMock);
+		}
+
+		[Test]
+		public void IsMatch_should_return_false_for_null_link()
+		{
+			// Arrange
+			HtmlLinkTag htmlTag = null;
+
+			// Act
+			bool actualMatch = _converter.IsMatch(htmlTag);
+
+			// Assert
+			Assert.False(actualMatch);
+		}
+
+		[Test]
+		[TestCase(null, false)]
+		[TestCase("", false)]
+		[TestCase("http://www.google.com", false)]
+		[TestCase("internal-link", false)]
+		[TestCase("special:MyPage", true)]
+		public void IsMatch_should_match_attachment_links(string href, bool expectedMatch)
+		{
+			// Arrange
+			var htmlTag = new HtmlLinkTag(href, href, "text", "");
+
+			// Act
+			bool actualMatch = _converter.IsMatch(htmlTag);
+
+			// Assert
+			Assert.AreEqual(actualMatch, expectedMatch);
+		}
+
+		[Test]
+		[TestCase("http://www.google.com", "http://www.google.com", false)]
+		[TestCase("internal-link", "internal-link", false)]
+		[TestCase("special:foofoo", "~/wiki/special:foofoo", true)]
+		public void Convert_should_change_expected_urls_to_full_paths(string href, string expectedHref, bool calledUrlHelper)
+		{
+			// Arrange
+			var originalTag = new HtmlLinkTag(href, href, "text", "");
+
+			// Act
+			var actualTag = _converter.Convert(originalTag);
+
+			// Assert
+			Assert.AreEqual(actualTag.OriginalHref, originalTag.OriginalHref);
+			Assert.AreEqual(actualTag.Href, expectedHref);
+			Assert.AreEqual(_urlHelperMock.ContentCalled, calledUrlHelper);
+		}
+	}
+}
