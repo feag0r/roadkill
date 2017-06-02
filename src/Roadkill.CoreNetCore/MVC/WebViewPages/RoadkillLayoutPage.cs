cb226a1899e67a03cd1b10939fb3e9633a098237
Chris S <chrisasmall+github@gmail.com>
2017-06-02 19:21:31 +0100

    MVC WebViewPages folder - WebViewPage is now RazorPage

--- src/Roadkill.CoreNetCore/MVC/WebViewPages/RoadkillLayoutPage.cs
+++ src/Roadkill.CoreNetCore/MVC/WebViewPages/RoadkillLayoutPage.cs
@@ -1,4 +1,5 @@
-﻿using Roadkill.Core.Configuration;
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Roadkill.Core.Configuration;
 using Roadkill.Core.DependencyResolution;
 using Roadkill.Core.Services;
 using Roadkill.Core.Text.TextMiddleware;
@@ -6,7 +7,7 @@ using Roadkill.Core.Text.TextMiddleware;
 namespace Roadkill.Core.Mvc.WebViewPages
 {
 	// Layout pages aren't created using IDependencyResolver (as they're outside of MVC). So use bastard injection for them.
-	public abstract class RoadkillLayoutPage : WebViewPage<object>
+	public abstract class RoadkillLayoutPage : RazorPage<object>
 	{
 		public ApplicationSettings ApplicationSettings { get; set; }
 		public IUserContext RoadkillContext { get; set; }
@@ -20,7 +21,7 @@ namespace Roadkill.Core.Mvc.WebViewPages
 
 			if (ApplicationSettings.Installed)
 			{
-                TextMiddlewareBuilder = LocatorStartup.Container.GetInstance<TextMiddlewareBuilder>();
+				TextMiddlewareBuilder = LocatorStartup.Container.GetInstance<TextMiddlewareBuilder>();
 				SiteSettings = LocatorStartup.Container.GetInstance<SettingsService>().GetSiteSettings();
 			}
 		}
--- src/Roadkill.CoreNetCore/MVC/WebViewPages/RoadkillViewPage.cs
+++ src/Roadkill.CoreNetCore/MVC/WebViewPages/RoadkillViewPage.cs
@@ -1,4 +1,5 @@
-﻿using Roadkill.Core.Configuration;
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Roadkill.Core.Configuration;
 using Roadkill.Core.Services;
 using Roadkill.Core.Text.Parsers;
 using Roadkill.Core.Text.TextMiddleware;
@@ -6,24 +7,24 @@ using StructureMap.Attributes;
 
 namespace Roadkill.Core.Mvc.WebViewPages
 {
-	public abstract class RoadkillViewPage<T> : WebViewPage<T>
+	public abstract class RoadkillViewPage<T> : RazorPage<T>
 	{
 		// Constructor injection isn't viable here, as this class is created by the ASP.NET runtime
 		private SiteSettings _siteSettings;
 
 		[SetterProperty]
 		public ApplicationSettings ApplicationSettings { get; set; }
-		
+
 		[SetterProperty]
 		public IUserContext RoadkillContext { get; set; }
-		
+
 		[SetterProperty]
 		public TextMiddlewareBuilder TextMiddlewareBuilder { get; set; }
 
-        [SetterProperty]
-        public IMarkupParser MarkupParser { get; set; }
+		[SetterProperty]
+		public IMarkupParser MarkupParser { get; set; }
 
-        [SetterProperty]
+		[SetterProperty]
 		public SettingsService SettingsService { get; set; }
 
 		public SiteSettings SiteSettings
